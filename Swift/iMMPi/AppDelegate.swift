e88fdecd3bf6c209dff2f450e886d0313be39e8b
wanderwaltz <wanderwaltz@gmail.com>
2014-08-24 18:52:01 +0700

    Added initial Swift project.

---
+++ Swift/iMMPi/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  iMMPi
+//
+//  Created by Egor Chiglintsev on 24.08.14.
+//  Copyright (c) 2014 Egor Chiglintsev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        return true
+    }
+}
+
