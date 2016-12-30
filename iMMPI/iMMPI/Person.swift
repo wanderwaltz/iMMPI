9b30c1bfc789956178007d7672e88d2705307f90
Egor Chiglintsev <wanderw@yandex-team.ru>
2016-12-30 18:23:27 +0700

    Add Person equality + tests.

--- iMMPI/iMMPI/Person.swift
+++ iMMPI/iMMPI/Person.swift
@@ -2,10 +2,37 @@ import Foundation
 
 /// A concrete implementation of `PersonProtocol`.
 final class Person: NSObject {
-    var name: String = ""
-    var gender: Gender = .male
-    var ageGroup: AgeGroup = .adult
+    var name: String
+    var gender: Gender
+    var ageGroup: AgeGroup
+
+    init(name: String, gender: Gender, ageGroup: AgeGroup) {
+        self.name = name
+        self.gender = gender
+        self.ageGroup = ageGroup
+    }
+
+    convenience override init() {
+        self.init(name: "", gender: .male, ageGroup: .adult)
+    }
 }
 
 
 extension Person: PersonProtocol {}
+
+
+extension Person {
+    override var hash: Int {
+        return name.hash ^ gender.hashValue ^ ageGroup.hashValue
+    }
+
+    override func isEqual(_ object: Any?) -> Bool {
+        guard let person = object as? Person else {
+            return false
+        }
+
+        return name == person.name
+            && gender == person.gender
+            && ageGroup == person.ageGroup
+    }
+}
--- iMMPI/iMMPITests/PersonTests.swift
+++ iMMPI/iMMPITests/PersonTests.swift
@@ -18,4 +18,58 @@ final class PersonTests: XCTestCase {
     func testThat__default_person_is_adult() {
         XCTAssertEqual(Person().ageGroup, .adult)
     }
+
+    func testThat__persons_with_all_matching_properties_are_equal() {
+        let person1 = Person(name: "John Doe", gender: .male, ageGroup: .adult)
+        let person2 = Person(name: "John Doe", gender: .male, ageGroup: .adult)
+
+        XCTAssertEqual(person1, person2)
+
+        XCTAssertTrue(person1.isEqual(person2))
+        XCTAssertTrue(person2.isEqual(person1))
+    }
+
+    func testThat__persons_with_all_matching_properties_have_equal_hashes() {
+        let person1 = Person(name: "John Doe", gender: .male, ageGroup: .adult)
+        let person2 = Person(name: "John Doe", gender: .male, ageGroup: .adult)
+
+        XCTAssertEqual(person1.hash, person2.hash)
+        XCTAssertEqual(person1.hashValue, person2.hashValue)
+    }
+
+    func testThat__persons_with_at_least_one_non_matching_property_are_not_equal() {
+        let persons = [
+            Person(name: "John Doe", gender: .male, ageGroup: .adult),
+            Person(name: "John Appleseed", gender: .male, ageGroup: .adult),
+            Person(name: "John Doe", gender: .female, ageGroup: .adult),
+            Person(name: "John Doe", gender: .male, ageGroup: .teen),
+            Person(name: "Mary Sue", gender: .female, ageGroup: .adult),
+            Person(name: "Mary Poppins", gender: .female, ageGroup: .teen)
+        ]
+
+        for first in persons {
+            for second in persons {
+                if first !== second {
+                    XCTAssertNotEqual(first, second)
+                    XCTAssertFalse(first.isEqual(second))
+                }
+            }
+        }
+    }
+
+    func testThat__person_is_not_equal_to_other_things() {
+        let person = Person(name: "John Doe", gender: .male, ageGroup: .adult)
+
+        XCTAssertFalse(person.isEqual("John Doe"))
+        XCTAssertFalse(person.isEqual(Gender.male))
+        XCTAssertFalse(person.isEqual(Gender.female))
+        XCTAssertFalse(person.isEqual(Gender.unknown))
+        XCTAssertFalse(person.isEqual(AgeGroup.adult))
+        XCTAssertFalse(person.isEqual(AgeGroup.teen))
+        XCTAssertFalse(person.isEqual(AgeGroup.unknown))
+        XCTAssertFalse(person.isEqual(123))
+        XCTAssertFalse(person.isEqual(true))
+        XCTAssertFalse(person.isEqual(NSObject()))
+        XCTAssertFalse(person.isEqual(nil))
+    }
 }
