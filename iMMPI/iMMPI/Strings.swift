ddd05ce44d0dfca3dee291c7a143411f457f8cd9
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-09 13:26:02 +0700

    Add more Swift string constants.

--- iMMPI/iMMPI/Strings.swift
+++ iMMPI/iMMPI/Strings.swift
@@ -3,6 +3,15 @@ import Foundation
 enum Strings {
     static let newRecord = NSLocalizedString("Новая запись", comment: "Заголовок экрана создания записи")
     static let editRecord = NSLocalizedString("Редактировать запись", comment: "Заголовок экрана редактирования записи")
+    static let selectDate = NSLocalizedString("Выберите дату", comment: "Заголовок экрана выбора даты")
 
     static let delete = NSLocalizedString("Удалить", comment: "Кнопка удаления")
+
+    static let ageGroupAdult = NSLocalizedString("Взрослая", comment: "Название возрастной группы")
+    static let ageGroupTeen = NSLocalizedString("Подростковая", comment: "Название возрастной группы")
+
+    static let genderMale = NSLocalizedString("Мужской", comment: "Пол")
+    static let genderFemale = NSLocalizedString("Женский", comment: "Пол")
+
+    static let unknown = NSLocalizedString("Неизвестно", comment: "Незивестное значение")
 }
