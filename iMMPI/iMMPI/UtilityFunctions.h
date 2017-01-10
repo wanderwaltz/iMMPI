e030ca2898297658653db8767b1620fec7318ed4
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-10 15:11:10 +0700

    Add nullability annotations in UtilityFunctions.

--- iMMPI/iMMPI/UtilityFunctions.h
+++ iMMPI/iMMPI/UtilityFunctions.h
@@ -6,17 +6,21 @@
 //  Copyright (c) 2012 Egor Chiglintsev. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import UIKit;
+
+NS_ASSUME_NONNULL_BEGIN
 
 /// Returns object if object is not nil, else returns [NSNull null]
-id nil2Null(id object);
+id nil2Null(id _Nullable object);
 
 /// Returns object if object is not of NSNull class, else returns nil
-id null2Nil(id object);
+id _Nullable null2Nil(id _Nullable object);
 
 
 
-UIViewController *SelfOrFirstChild(UIViewController *controller);
+UIViewController * _Nullable SelfOrFirstChild(UIViewController * _Nullable controller);
 
 
-NSString *TransliterateToLatin(NSString *string);
+NSString * _Nullable TransliterateToLatin(NSString * _Nullable string);
+
+NS_ASSUME_NONNULL_END
