a3538bdf52ff9bce112bb2c93453f05aecd8ce4b
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-10 15:10:56 +0700

    Make PersonProtocol inherit from NSObjectProtocol.

--- iMMPI/iMMPI/PersonProtocol.swift
+++ iMMPI/iMMPI/PersonProtocol.swift
@@ -3,7 +3,7 @@ import Foundation
 /// Encapsulates personal information of a person taking MMPI test.
 ///
 /// Gender and the age group are relevant for selecting a proper questionnaire for the person.
-@objc protocol PersonProtocol {
+@objc protocol PersonProtocol: NSObjectProtocol {
     /// Full name of a person.
     var name: String { get set }
 
