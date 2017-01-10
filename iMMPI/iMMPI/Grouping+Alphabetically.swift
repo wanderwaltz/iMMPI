fe4df3a4259b61332f1d1b18bb4372e2801d2586
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-10 15:10:32 +0700

    Make alphabetical grouping case insensitive.

--- iMMPI/iMMPI/Grouping+Alphabetically.swift
+++ iMMPI/iMMPI/Grouping+Alphabetically.swift
@@ -4,7 +4,7 @@ extension Array {
     func groupAlphabetically(by title: @escaping (Element) -> String) -> Grouping<Element> {
         return Grouping(
             items: self,
-            areInIncreasingOrder: { title($0) < title($1) },
+            areInIncreasingOrder: { title($0).lowercased() < title($1).lowercased() },
             sectionDescriptor: SectionDescriptor(
                 itemsBelongToSameSection: { title($0).uppercasedFirstCharacter == title($1).uppercasedFirstCharacter },
                 sectionTitleForItem: { title($0).uppercasedFirstCharacter }
