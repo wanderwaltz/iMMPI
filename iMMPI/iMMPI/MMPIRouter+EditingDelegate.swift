98a4c51e98853044ed6e5ea7f793d2964f242be6
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-11 14:57:43 +0700

    Extract EditTestRecordViewControllerDelegate into an extension.

--- iMMPI/iMMPI/MMPIRouter+EditingDelegate.swift
+++ iMMPI/iMMPI/MMPIRouter+EditingDelegate.swift
@@ -1,36 +1,37 @@
 import Foundation
 
 extension MMPIRouter {
-    final class EditingDelegate: NSObject, EditTestRecordViewControllerDelegate {
-        func editTestRecordViewController(_ controller: EditTestRecordViewController,
-                                          didFinishEditing record: TestRecordProtocol?) {
-            controller.dismiss(animated: true, completion: nil)
-
-            if let record = record {
-                if storage.contains(record) {
-                    storage.update(record)
-                }
-                else {
-                    storage.add(record)
-                }
-
-                NotificationCenter.default.post(
-                    Notification(
-                        name: .didEditRecord,
-                        object: record,
-                        userInfo: nil
-                    )
-                )
-            }
-        }
-
-
+    final class EditingDelegate: NSObject {
         init(storage: TestRecordStorage) {
             self.storage = storage
         }
 
-
         fileprivate let storage: TestRecordStorage
+    }
+}
+
+
+extension MMPIRouter.EditingDelegate: EditTestRecordViewControllerDelegate {
+    func editTestRecordViewController(_ controller: EditTestRecordViewController,
+                                      didFinishEditing record: TestRecordProtocol?) {
+        controller.dismiss(animated: true, completion: nil)
+
+        if let record = record {
+            if storage.contains(record) {
+                storage.update(record)
+            }
+            else {
+                storage.add(record)
+            }
+
+            NotificationCenter.default.post(
+                Notification(
+                    name: .didEditRecord,
+                    object: record,
+                    userInfo: nil
+                )
+            )
+        }
     }
 }
 
