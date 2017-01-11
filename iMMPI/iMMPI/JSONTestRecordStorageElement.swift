943cc2a2e4d1ca3690331cf2e3ec734bb8897f7a
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-11 15:31:04 +0700

    Rewrite JSONTestRecordStorageElement in Swift.

---
+++ iMMPI/iMMPI/JSONTestRecordStorageElement.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+final class JSONTestRecordStorageElement: NSObject {
+    var record: TestRecordProtocol?
+    var fileName: String?
+}
--- iMMPI/iMMPI/JSONTestRecordsStorage.m
+++ iMMPI/iMMPI/JSONTestRecordsStorage.m
@@ -11,7 +11,6 @@
 #endif
 
 #import "JSONTestRecordsStorage.h"
-#import "JSONTestRecordStorageElement.h"
 #import "JSONTestRecordProxy.h"
 
 
