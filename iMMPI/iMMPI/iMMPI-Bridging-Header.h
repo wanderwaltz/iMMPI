b5aac2d149a8d910ad06c80b4d8a297eeb5a8a24
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-12 14:38:30 +0700

    Update bridging header.

--- iMMPI/iMMPI/iMMPI-Bridging-Header.h
+++ iMMPI/iMMPI/iMMPI-Bridging-Header.h
@@ -5,5 +5,10 @@
 #import "Storyboard.h"
 
 #import "AnalysisViewController.h"
+#import "AnalyzerTableViewCell.h"
+#import "AnalysisOptionsViewController.h"
+#import "AnalyzerGroupDetailedInfoViewController.h"
+#import "AnalysisSettings.h"
+#import "AnalysisHTMLReportGenerator.h"
 
 #import "StatementTableViewCell.h"
