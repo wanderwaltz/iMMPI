07a87af3167146e48eb6549c348b45c46430483a
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-13 09:52:20 +0700

    Add tests for UsingRouter+NSObject.

---
+++ iMMPI/iMMPITests/StubRouter.swift
@@ -0,0 +1,15 @@
+@testable import iMMPI
+
+final class StubRouter: Router {
+    func displayAllRecords(sender: UIViewController) {}
+    func displayTrash(sender: UIViewController) {}
+
+    func displayAnalysis(for record: TestRecordProtocol, sender: UIViewController) {}
+    func displayAnswersReview(for record: TestRecordProtocol, sender: UIViewController) {}
+
+    func displayDetails(for group: TestRecordsGroup, sender: UIViewController) throws {}
+
+    func addRecord(basedOn record: TestRecordProtocol, sender: UIViewController) throws {}
+    func edit(_ record: TestRecordProtocol, sender: UIViewController) throws {}
+    func displayAnswersInput(for record: TestRecordProtocol, sender: UIViewController) throws {}
+}
