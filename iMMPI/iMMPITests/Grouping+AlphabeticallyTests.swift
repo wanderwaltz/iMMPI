2b1123f1d9529bb1ac19219e3a7a55c60bb9dde6
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-13 10:32:45 +0700

    Add tests for Grouping+Alphabetically.

---
+++ iMMPI/iMMPITests/Grouping+AlphabeticallyTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import iMMPI
+
+final class GroupingAlphabeticallyTests: XCTestCase {
+    func test__grouping_alphabetically() {
+        let grouping = ["abbA", "boris", "Burger", "bAd", "Corn", "cake"].groupAlphabetically(by: { $0 })
+        XCTAssertEqual(grouping.allItems, ["abbA", "bAd", "boris", "Burger", "cake", "Corn"])
+
+        XCTAssertEqual(grouping.numberOfSections, 3)
+
+        XCTAssertEqual(grouping.title(forSection: 0), "A")
+        XCTAssertEqual(grouping.title(forSection: 1), "B")
+        XCTAssertEqual(grouping.title(forSection: 2), "C")
+
+        XCTAssertEqual(grouping.item(at: IndexPath(row: 0, section: 0)), "abbA")
+        XCTAssertEqual(grouping.item(at: IndexPath(row: 0, section: 1)), "bAd")
+        XCTAssertEqual(grouping.item(at: IndexPath(row: 1, section: 1)), "boris")
+        XCTAssertEqual(grouping.item(at: IndexPath(row: 2, section: 1)), "Burger")
+        XCTAssertEqual(grouping.item(at: IndexPath(row: 0, section: 2)), "cake")
+        XCTAssertEqual(grouping.item(at: IndexPath(row: 1, section: 2)), "Corn")
+    }
+}
