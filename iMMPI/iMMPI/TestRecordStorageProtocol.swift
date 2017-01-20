79924b0ad3d4a4b9bd1cfc7a89a739c1f2e42544
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-20 14:06:25 +0700

    Log time taken to load records.

--- iMMPI/iMMPI/TestRecordStorageProtocol.swift
+++ iMMPI/iMMPI/TestRecordStorageProtocol.swift
@@ -11,6 +11,7 @@ extension TestRecordStorage {
         -> RecordsListViewModel<TestRecordProtocol> {
             return RecordsListViewModel(
                 provider: AsyncProvider({ completion in
+                    let startTimestamp = Date()
                     DispatchQueue.global().async {
                         if self.isEmpty {
                             self.load()
@@ -19,6 +20,8 @@ extension TestRecordStorage {
                         let records = self.all().filter(includeRecord)
 
                         DispatchQueue.main.async {
+                            let endTimestamp = Date()
+                            NSLog("Loaded \(records.count) records in \(endTimestamp.timeIntervalSince(startTimestamp)/1000.0) seconds")
                             completion(records)
                         }
                     }
