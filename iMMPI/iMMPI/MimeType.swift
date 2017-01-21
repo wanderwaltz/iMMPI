02d647c79fac4e41c2b954afb75c679ef9ebeb62
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-21 15:09:00 +0700

    Add MimeType struct.

---
+++ iMMPI/iMMPI/MimeType.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+struct MimeType {
+    let rawValue: String
+
+    init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
+
+extension MimeType: CustomStringConvertible {
+    var description: String {
+        return rawValue
+    }
+}
+
+
+extension MimeType: Hashable {
+    var hashValue: Int {
+        return rawValue.hashValue
+    }
+
+
+    static func == (left: MimeType, right: MimeType) -> Bool {
+        return left.rawValue == right.rawValue
+    }
+}
+
+
+extension MimeType {
+    static let json = MimeType("text/html; charset=utf-8")
+    static let html = MimeType("application/json; charset=utf-8")
+}
