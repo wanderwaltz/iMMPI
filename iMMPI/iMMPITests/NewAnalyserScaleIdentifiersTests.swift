b2edef30adb6f04af6081d208dfa50e4708a1d12
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-01-23 03:19:26 +0700

    Add test that all scales have unique titles.

--- iMMPI/iMMPITests/NewAnalyserScaleIdentifiersTests.swift
+++ iMMPI/iMMPITests/NewAnalyserScaleIdentifiersTests.swift
@@ -9,9 +9,15 @@ final class NewAnalyserScaleIdentifiersTests: XCTestCase {
         analyser = NewAnalyser()
     }
 
-    func testThat__all_nested_scales_have_unique_identifiers() {
+    func testThat__all_scales_have_unique_identifiers() {
         let identifiers = Set(analyser.scales.map({ $0.identifier }))
 
         XCTAssertEqual(identifiers.count, analyser.scales.count)
+    }
+
+    func testThat__all_scales_have_unique_titles() {
+        let titles = Set(analyser.scales.map({ $0.title }))
+
+        XCTAssertEqual(titles.count, analyser.scales.count)
     }
 }
