4b0a95c300106104de5d9af4d419e7f8d606f425
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-02-10 17:24:49 +0700

    Add fonts.

--- iMMPI/iMMPI/Fonts.swift
+++ iMMPI/iMMPI/Fonts.swift
@@ -2,4 +2,8 @@ import UIKit
 
 enum Fonts {
     static let solidButtonTitle = UIFont.preferredFont(forTextStyle: .body)
+
+    static let scaleGroupTitle = UIFont.preferredFont(forTextStyle: .title3)
+    static let majorScaleTitle = UIFont.preferredFont(forTextStyle: .body)
+    static let minorScaleTitle = UIFont.preferredFont(forTextStyle: .subheadline)
 }
