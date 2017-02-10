10d96c30561fb490dca45bd3dc3f1d68716f95e6
Egor Chiglintsev <wanderw@yandex-team.ru>
2017-02-10 17:24:29 +0700

    Add separator color. Update colors.

--- iMMPI/iMMPI/Colors.swift
+++ iMMPI/iMMPI/Colors.swift
@@ -2,5 +2,7 @@ import UIKit
 
 enum Colors {
     static let solidButtonBackground = UIColor(white: 0.98, alpha: 1)
-    static let solidButtonBorder = UIColor(white: 0.92, alpha: 1)
+    static let solidButtonBorder = UIColor(white: 224.0 / 255.0, alpha: 1)
+
+    static let separator = Colors.solidButtonBorder
 }
