e5fd26c0499f79a14e7a676985126a6cea662e02
Egor Chiglintsev <wanderw@yandex-team.ru>
2018-09-17 16:53:31 +0700

    Add more fonts and colors settings.

--- iMMPI/iMMPI/UI/Colors.swift
+++ iMMPI/iMMPI/UI/Colors.swift
@@ -5,4 +5,6 @@ enum Colors {
     static let solidButtonBorder = UIColor(white: 224.0 / 255.0, alpha: 1)
 
     static let separator = Colors.solidButtonBorder
+
+    static let darkText = UIColor.darkText
 }
--- iMMPI/iMMPI/UI/Fonts.swift
+++ iMMPI/iMMPI/UI/Fonts.swift
@@ -6,4 +6,6 @@ enum Fonts {
     static let scaleGroupTitle = UIFont.preferredFont(forTextStyle: .title3)
     static let majorScaleTitle = UIFont.preferredFont(forTextStyle: .body)
     static let minorScaleTitle = UIFont.preferredFont(forTextStyle: .subheadline)
+
+    static let analysisDateHeaderTitle = UIFont.preferredFont(forTextStyle: .caption1)
 }
