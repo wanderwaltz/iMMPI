aa7636ee4cbbdf1526c684cd08d4307eddfef46c
Egor Chiglintsev <wanderw@yandex-team.ru>
2018-09-22 13:37:40 +0700

    Update html report CSS and email message text.

--- iMMPI/iMMPI/Model/Analysis/Reports/Email/EmailTextGenerator.swift
+++ iMMPI/iMMPI/Model/Analysis/Reports/Email/EmailTextGenerator.swift
@@ -23,8 +23,32 @@ extension EmailTextGenerator: AnalysisReportGenerator {
 
 extension EmailTextGenerator {
     static let `default` = EmailTextGenerator({ result in
-        // TODO: include full profile (base scales)
-        let scale = AnalysisScale.f
-        return "\(scale.title): \(scale.formatter.format(scale.score.value(for: result.record)))"
+        let baseScales: [AnalysisScale] = [
+            .l,  // L. Ложь (неискренность)
+            .f,  // F. Достоверность (невалидность)
+            .k,  // K. Коррекция
+            .hs, // 1. Ипохондрия (Сверхконтроль)
+            .d,  // 2. Депрессия (Пессимистичность)
+            .hy, // 3. Истерия
+            .pd, // 4. Психопатия (Импульсивность)
+            .mf, // 5. Половое развитие
+            .pa, // 6. Паранойя (Ригидность)
+            .pf, // 7. Психастения (Тревожность)
+            .sc, // 8. Шизофрения (индивидуалистичность)
+            .ma, // 9. Гипомания (оптимистичность)
+            .si  // 0. Интраверсия
+        ]
+
+        var lines: [String] = []
+
+        lines.append("\(result.personName) \(DateFormatter.medium.string(from: result.date))")
+        lines.append("")
+        lines.append(
+            contentsOf: baseScales.map({ scale in
+                "\(scale.title): \(scale.formatter.format(scale.score.value(for: result.record)))"
+            })
+        )
+
+        return lines.joined(separator: "\n")
     })
 }
